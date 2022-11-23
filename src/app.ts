8b35c91c584259e2cd7d0d6a33034decd440a17c
Xilong <xilong1393@sina.com>
2022-11-23 09:18:14 -0600

    added environment variables for datadog

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
-import tracer from "dd-trace";
-tracer.init({ logInjection: true }); // initialized in a different file to avoid hoisting.
-import dotenv from 'dotenv'
-dotenv.config()
+import "./helper/config"
+import "./helper/tracer";
 
 import { FetchAwardsServer } from './server';
 import Database from './dao/database';
---
+++ src/helper/config.ts
@@ -0,0 +1,6 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+console.log("dotenv init")
+
+// export default dotenv
