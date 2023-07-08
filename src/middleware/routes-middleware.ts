779fee016229068bbf667374a4f58900c526bbfe
Xilong <xilong1393@sina.com>
2023-07-08 10:54:59 -0400

    health endpoint

--- src/middleware/routes-middleware.ts
+++ src/middleware/routes-middleware.ts
@@ -9,7 +9,7 @@ import PointService from "../service/point-service";
 import PointToConsume from "../model/point-to-consume";
 import PointEvent from "../model/point-event";
 import * as _ from "lodash";
-import { SystemError, TreeSet } from "tstl";
+import { SystemError, TreeMap, TreeSet } from "tstl";
 import PointBalance from "../model/point-balance";
 import { UserService } from "../service/user-service";
 import { logger } from "../helper/winston-logger";
@@ -26,6 +26,10 @@ export class Routes {
   }
   private setup() {
     this.app.use("/v1", this.router);
+
+    this.router.get("/health", (req: Request, res: Response, next: NextFunction) => {
+      res.send({ health: "ok" });
+    });
 
     // get all balances summary
     this.router.get(
@@ -50,32 +54,65 @@ export class Routes {
       "/balances/test",
       async (req: Request, res: Response, next: NextFunction) => {
         try {
-          const treeset: TreeSet<PointBalance> = new TreeSet<PointBalance>(
-            (a, b) =>
-              a.lastUpdated.getTime() != b.lastUpdated.getTime()
-                ? a.lastUpdated < b.lastUpdated
-                : a.payer < b.payer
+          // const treeset: TreeSet<PointBalance> = new TreeSet<PointBalance>(
+          //   (a, b) =>
+          //     a.lastUpdated.getTime() != b.lastUpdated.getTime()
+          //       ? a.lastUpdated < b.lastUpdated
+          //       : a.payer < b.payer
+          // );
+          const treeMap: TreeMap<PointBalance, PointBalance> = new TreeMap<
+            PointBalance,
+            PointBalance
+          >((a, b) =>
+            a.lastUpdated.getTime() != b.lastUpdated.getTime()
+              ? a.lastUpdated < b.lastUpdated
+              : a.payer < b.payer
           );
 
-          treeset.insert(
+          treeMap.set(
+            new PointBalance("test2", 100, new Date("2022-10-28T10:00:00Z")),
             new PointBalance("test2", 100, new Date("2022-10-28T10:00:00Z"))
           );
-          treeset.insert(
+          treeMap.set(
+            new PointBalance("test1", 120, new Date("2022-10-28T10:00:00Z")),
             new PointBalance("test1", 120, new Date("2022-10-28T10:00:00Z"))
           );
-          treeset.insert(
-            new PointBalance("test3", 90, new Date("2022-11-28T10:00:00Z"))
+          treeMap.set(
+            new PointBalance("test3", 90, new Date("2022-11-28T10:00:00Z")),
+            new PointBalance("test1", 120, new Date("2022-10-28T10:00:00Z"))
           );
-          treeset.insert(
-            new PointBalance("test3", 90, new Date("2022-11-03T10:00:00Z"))
+          treeMap.emplace(
+            new PointBalance("test3", 90, new Date("2022-11-03T10:00:00Z")),
+            new PointBalance("test1", 120, new Date("2022-10-28T10:00:00Z"))
           );
-          treeset.insert(
-            new PointBalance("test1", 100, new Date("2022-11-04T10:00:00Z"))
+          treeMap.set(
+            new PointBalance("test1", 100, new Date("2022-11-04T10:00:00Z")),
+            new PointBalance("test1", 120, new Date("2022-10-28T10:00:00Z"))
           );
           // treeset.erase(new PointBalance("test2", 100, new Date("2022-10-28T10:00:00Z")))
-          const result = treeset.has(
+          const result = treeMap.has(
             new PointBalance("test2", 200, new Date("2022-10-28T10:00:00Z"))
           );
+
+          // treeset.insert(
+          //   new PointBalance("test2", 100, new Date("2022-10-28T10:00:00Z"))
+          // );
+          // treeset.insert(
+          //   new PointBalance("test1", 120, new Date("2022-10-28T10:00:00Z"))
+          // );
+          // treeset.insert(
+          //   new PointBalance("test3", 90, new Date("2022-11-28T10:00:00Z"))
+          // );
+          // treeset.insert(
+          //   new PointBalance("test3", 90, new Date("2022-11-03T10:00:00Z"))
+          // );
+          // treeset.insert(
+          //   new PointBalance("test1", 100, new Date("2022-11-04T10:00:00Z"))
+          // );
+          // // treeset.erase(new PointBalance("test2", 100, new Date("2022-10-28T10:00:00Z")))
+          // const result = treeset.has(
+          //   new PointBalance("test2", 200, new Date("2022-10-28T10:00:00Z"))
+          // );
           res.send(result);
         } catch (error) {
           next(error);
