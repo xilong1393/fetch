0f605cc989a0ed6f6ec8038b2192e41335851413
SamYu <xilong1393@sina.com>
2023-07-24 15:16:17 -0400

    process.env

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,8 @@ import "./helper/tracer";
 import { FetchAwardsServer } from './server';
 import Database from './dao/database';
 
+console.log(process.env.test)
+
 const database = new Database();
 database.connect();
 const server = new FetchAwardsServer();
